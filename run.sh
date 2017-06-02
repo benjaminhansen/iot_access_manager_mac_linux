4911ea79e24875d4daca4b7c524c72d1333832b6
bjhansen <bjhansen@ualr.edu>
2017-06-02 14:51:41 -0500

    Directory restructure

--- run.sh
+++ run.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-platform='unknown'
-unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-   platform='linux'
-elif [[ "$unamestr" == 'Darwin' ]]; then
-   platform='mac'
-fi
-
-assets/php/$platform/bin/php ./assets/laravel/iotmgmt/artisan iot:register
+app/php/unix/bin/php ./app/laravel/iotmgmt/artisan iot:register
