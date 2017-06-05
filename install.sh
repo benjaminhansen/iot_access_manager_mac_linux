23906450bc1289368b43d11e3799f78cace78f03
bjhansen <bjhansen@ualr.edu>
2017-06-05 11:29:52 -0500

    Updated installation process

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s app/laravel/iotmgmt/vendor vendor
+
+echo "#!/bin/bash" >> run.sh
+echo "app/php/unix/bin/php ./app/laravel/iotmgmt/artisan iot:register" >> run.sh
+
+chmod +x run.sh
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-app/php/unix/bin/php ./app/laravel/iotmgmt/artisan iot:register
